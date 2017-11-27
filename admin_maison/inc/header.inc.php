1343e6c7cc43f0cb9d85e50c8a4e53527fa0ae7e
Unknown <sebastien.miatti@lepoles.com>
2017-11-27 23:38:10 +0100

    modif maison

--- admin_maison/inc/header.inc.php
+++ admin_maison/inc/header.inc.php
@@ -38,11 +38,12 @@ $ligne_utilisateur = $resultat -> fetch();
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
-                <li class="active"><a href="utilisateurs.php">Profil<span class="sr-only">(current)</span></a></li>
-                <li><a href="modification_utilisateur.php">gestion utilisateur</a></li>
+                <li class="active"><a href="utilisateur.php">Profil<span class="sr-only">(current)</span></a></li>
+
                 <li class="dropdown">
                     <a href="" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">parcours<span class="caret"></span></a>
                     <ul class="dropdown-menu">
+                        <li><a href="competence.php">Compétences</a></li>
                         <li><a href="experience.php">Expériences</a></li>
                         <li><a href="realisation.php">Réalisations</a></li>
                         <li><a href="formation.php">Formations</a></li>
@@ -50,26 +51,11 @@ $ligne_utilisateur = $resultat -> fetch();
                         <li><a href="loisirs.php">Loisirs</a></li>
                     </ul>
                 </li>
+
             </ul>
 
-        <form class="navbar-form navbar-left">
-            <div class="form-group">
-                <input type="text" class="form-control" placeholder="Search">
-            </div>
-            <button type="submit" class="btn btn-default">Submit</button>
-        </form>
             <ul class="nav navbar-nav navbar-right">
                 <li><a href="#">Se déconnecter</a></li>
-                    <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Options d'adminitration<span class="caret"></span></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="utilisateur.php">infomations utilisateurs</a></li>
-                            <li><a href="#">Another action</a></li>
-                            <li><a href="#">Something else here</a></li>
-                            <li role="separator" class="divider"></li>
-                            <li><a href="#">Separated link</a></li>
-                        </ul>
-                    </li>
             </ul>
         </div><!-- /.navbar-collapse -->
     </div><!-- /.container-fluid -->
---
+++ admin_maison/utilisateur.php
@@ -0,0 +1,98 @@
+<?php
+//inclusion du header comprenant l'init
+include('inc/header.inc.php');
+
+// gestion des contenus de la BDD utilisateur
+$resultat = $pdo -> prepare("SELECT * FROM t_utilisateurs WHERE id_utilisateur='1'");
+$resultat->execute();
+$nbr_utilisateur = $resultat->rowCount();// $ligne_realisation = $resultat -> fetch();
+
+
+// insertion d'un utilisateur
+if (isset($_POST['utilisateur']))
+    { // Si on a posté un nouveau utilisateur.
+        if (!empty($_POST['utilisateur']))
+            { // Si l'entrée d'un utilisateur n'est pas vide
+                $utilisateur = addslashes($_POST['utilisateur']);
+                $prenom = addslashes($_POST['prenom']);
+                $nom = addslashes($_POST['nom']);
+                $email = addslashes($_POST['email']);
+                $telephone = addslashes($_POST['telephone']);
+                $mdp = addslashes($_POST['mdp']);
+                $pseudo = addslashes($_POST['pseudo']);
+                $avatar = addslashes($_POST['avatar']);
+                $age = addslashes($_POST['age']);
+                $date_de_naissance = addslashes($_POST['date_de_naissance']);
+                $sexe = addslashes($_POST['sexe']);
+                $etat_civil = addslashes($_POST['etat_civil']);
+                $adresse = addslashes($_POST['adresse']);
+                $code_postal = addslashes($_POST['code_postal']);
+                $ville = addslashes($_POST['ville']);
+                $pays = addslashes($_POST['pays']);
+                $site_web = addslashes($_POST['site_web']);
+                $pdo -> exec("INSERT INTO t_utilisateurs VALUES (NULL, '$utilisateur', '$prenom', '$nom', '$email', '$telephone', '$mdp', '$pseudo', '$avatar', '$age', '$etat_civil', '$sexe', '$etat_civil', '$adresse', '$code_postal', '$ville', '$pays', '$site_web', '1')");
+                //mettre $id_utilisateur quand on l'aura dans la variable de session
+                header("location: utilisateur.php");
+                exit();
+            } // ferme le if n'est pas vide
+    } // ferme le if(isset) du form
+
+// Suppression d'un utilisateur
+if (isset($_GET['id_utilisateur'])) { // on récupère la comp. par son id dans l'url
+    $efface =  $_GET['id_utilisateur'];
+    $resultat = "DELETE FROM t_utilisateurs WHERE id_utilisateur = '$efface'";
+    $pdo -> query($resultat); // on peut avec exec aussi si on veut
+    header("location: utilisateur.php"); // pour revenir sur la page
+} // ferme le if(isset)
+
+?>
+
+<div class="panel panel-info">
+    <div class="panel-heading text-center"><b>Liste des Utilisateurs</b></div>
+</div>
+
+
+<div class="col-sm-4 col-md-5">
+    <div class="thumbnail">
+
+        <div class="caption">
+            <div class="panel panel-info">
+                <div class="panel-heading">J'ai <?= $nbr_utilisateur;?> utilisateur<?= ($nbr_utilisateur>1)?'s' : ''?></div>
+            </div>
+            <img src="..." alt="..."/>
+                <?php while ($ligne_utilisateur = $resultat -> fetch()) : ?>
+                    <h3><?= $ligne_utilisateur['nom'];?> <?= $ligne_utilisateur['prenom'];?></h3>
+                    <p><?= $ligne_utilisateur['pseudo'];?></p>
+                    <p><?= $ligne_utilisateur['nom'];?></p>
+                    <p><?= $ligne_utilisateur['prenom'];?></p>
+                    <p class="hidden"><?= $ligne_utilisateur['id_utilisateur'];?></p>
+                    <p><?= $ligne_utilisateur['email'];?></p>
+                    <p><?= $ligne_utilisateur['telephone'];?></p>
+                    <p><?= $ligne_utilisateur['mdp'];?></p>
+                    <p><?= $ligne_utilisateur['pseudo'];?></p>
+                    <p><?= $ligne_utilisateur['age'];?></p>
+                    <p><?= $ligne_utilisateur['date_de_naissance'];?></p>
+                    <p><?= $ligne_utilisateur['sexe'];?></p>
+                    <p><?= $ligne_utilisateur['etat_civil'];?></p>
+                    <p><?= $ligne_utilisateur['adresse'];?></p>
+                    <p><?= $ligne_utilisateur['code_postal'];?></p>
+                    <p><?= $ligne_utilisateur['ville'];?></p>
+                    <p><?= $ligne_utilisateur['pays'];?></p>
+                    <p><?= $ligne_utilisateur['site_web'];?></p>
+
+                <td><a href="modification_utilisateur.php?id_utilisateur=<?= $ligne_utilisateur['id_utilisateur'];?>"><button type="button" class="btn btn-warning"><span class="glyphicon glyphicon-pencil" aria-hidden="true">Modifier</span></button></a></td>
+                <td><a href="utilisateur.php?id_utilisateur=<?= $ligne_utilisateur['id_utilisateur'];?>"><button type="button" class="btn btn-danger"><span class="glyphicon glyphicon-pencil" aria-hidden="true">Supprimer</span></button></a></td>
+
+                <?php endwhile ?>
+        </div>
+    </div>
+</div>
+
+
+</div>
+
+        <div class="panel-footer text-center">
+            <a href="index.php">Retour à la page d'accueil</a>
+        </div>
+
+    <?php require('inc/footer.inc.php');?>
