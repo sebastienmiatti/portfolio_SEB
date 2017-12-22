20189ca2751494279fc7668a5df66f9535b9f85f
Unknown <sebastien.miatti@lepoles.com>
2017-12-22 12:54:32 +0100

    commit41

---
+++ colo/admin/texte.php
@@ -0,0 +1,130 @@
+<?php
+
+// inclusions de l'init
+require('inc/init.inc.php');
+// gestion des contenus de la BDD compétences
+
+// if(isset($_SESSION['connexion']) && $_SESSION['connexion'] == 'connecté'){
+//     $id_utilisateur = $_SESSION['id_utilisateur'];
+//     $prenom = $_SESSION['prenom'];
+//     $nom = $_SESSION['nom'];
+//
+// }else{
+//     header('location: connexion.php');
+// }
+
+// insertion de textes
+if (isset($_POST['t_head']))
+    { // Si on a posté un nouveau texte.
+    if ($_POST['t_head']!='' && $_POST['t_body']!='' && $_POST['t_foot']!='')
+        { // Si réalisation n'est pas vide
+            $t_head = addslashes($_POST['t_head']);
+            $t_body = addslashes($_POST['t_body']);
+            $t_foot = addslashes($_POST['t_foot']);
+            $pdo->exec(" INSERT INTO t_textes VALUES (NULL, '$t_head', '$t_body', '$t_foot', '1')"); // mettre $id_utilisateur quand on l'aura dans la variable de session
+            header("location: texte.php");
+            exit();
+        } // ferme le if n'est pas vide
+    } // ferme le if(isset) du form
+
+// pas de suppression des texte du CV on prends le dernier à jour
+
+		$sql = $pdo->query(" SELECT * FROM t_textes WHERE utilisateur_id ='1' ORDER BY id_texte DESC LIMIT 1 ");
+		$ligne_texte = $sql->fetch();
+
+//inclusion du header
+require('inc/header.inc.php');
+
+?>
+
+  <hr>
+  		<div class="panel panel-info">
+      		<div class="panel-heading text-center"><b>Derniers paragraphes de texte</b></div>
+  		</div>
+
+   		<div class="row">
+    		<div class="text-justify col-sm-4 col-md-8">
+	    		<div class="panel panel-info">
+		 			<div class="panel-body">
+						<table class="table table-bordered table-hover" border="2">
+							<thead>
+								<tr>
+									<th>Texte d'en-tete</th>
+									<th>Texte de corps</th>
+									<th>Texte du bas</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+                                    <td><?= $ligne_texte['t_head'];?></td>
+                                    <td><?= $ligne_texte['t_body'];?></td>
+                                    <td><?= $ligne_texte['t_foot'];?></td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</div>
+   			</div>
+
+    		<div class="col-sm-4 col-lg-4 text-justify">
+				<div class="panel panel-primary">
+				<div class="panel-heading">Insertion et mise à jour des textes de la page</div>
+	                <div class="panel-body">
+		<!--formulaire d'insertion-->
+					<form action="texte.php" method="post">
+                        <div class="form-group">
+                            <label for="t_head">Texte d'en-tête:</label>
+                            <textarea type="text" name="t_head" class="form-control"><?= $ligne_texte['t_head']; ?></textarea>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="t_body">Texte de corps:</label>
+                            <textarea type="text" name="t_body" class="form-control"><?= $ligne_texte['t_body']; ?></textarea>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="t_foot">Texte de bas de page:</label>
+                            <textarea type="text" name="t_foot" class="form-control"><?= $ligne_texte['t_foot']; ?></textarea>
+                        </div>
+
+
+                        <button type="submit" class="btn btn-success btn-block">Insérez les infos</button>
+
+					</form>
+				</div>
+			</div>
+			</div>
+		</div>
+  	<hr>
+
+  	<!-- <div class="row">
+    	<div class="text-center col-md-12">
+      	<div class="well"><strong>Composants Bootstrap de base</strong></div>
+    	</div>
+  	</div> -->
+  	<!-- <div class="row">
+    	<div class="col-sm-4 text-center">
+      		<h4>Boutons</h4>
+      		<p>Quickly add buttons to your page by using the button component in the insert panel. </p>
+      		<button type="button" class="btn btn-info btn-sm">Info bouton</button>
+      		<button type="button" class="btn btn-success btn-sm">Success bouton</button>
+    	</div>
+    	<div class="text-center col-sm-4">
+      		<h4>Labels ou étiquettes Bootstrap</h4>
+      		<p>Using the insert panel, add labels to your page by using the label component.</p>
+      		<span class="label label-warning">Info Label</span>
+			<span class="label label-danger">Danger Label</span>
+		</div>
+    	<div class="text-center col-sm-4">
+      		<h4><strong>Glyphicons</strong></h4>
+      		<p>You can also add glyphicons to your page from within the insert panel.</p>
+      		<div class="row">
+        		<div class="col-xs-4"><span class="glyphicon glyphicon-menu-hamburger" aria-hidden="true"></span></div>
+        		<div class="col-xs-4"><span class="glyphicon glyphicon-home" aria-hidden="true"> </span> </div>
+        		<div class="col-xs-4"><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span></div>
+      		</div>
+    	</div>
+  	</div> -->
+  	<!-- <hr> -->
+		  <!--	 footer en include-->
+	<?php include("inc/footer.inc.php"); ?>
---
+++ colo/admin/utilisateur.php
@@ -0,0 +1,112 @@
+<?php
+
+//inclusion de l'init
+require('inc/init.inc.php');
+
+// if(isset($_SESSION['connexion']) && $_SESSION['connexion'] == 'connecté'){
+//     $id_utilisateur = $_SESSION['id_utilisateur'];
+//     $prenom = $_SESSION['prenom'];
+//     $nom = $_SESSION['nom'];
+//
+// }else{
+//     header('location: connexion.php');
+// }
+
+// gestion des contenus de la BDD utilisateur
+$resultat = $pdo -> prepare("SELECT * FROM t_utilisateurs WHERE id_utilisateur='1'");
+$resultat->execute();
+$nbr_utilisateur = $resultat->rowCount();
+// $ligne_realisation = $resultat -> fetch();
+
+
+// insertion d'un utilisateur
+if (isset($_POST['utilisateur']))
+    { // Si on a posté un nouveau utilisateur.
+        if (!empty($_POST['utilisateur']))
+            { // Si l'entrée d'un utilisateur n'est pas vide
+                $utilisateur = addslashes($_POST['utilisateur']);
+                $prenom = addslashes($_POST['prenom']);
+                $nom = addslashes($_POST['nom']);
+                $email = addslashes($_POST['email']);
+                $telephone = addslashes($_POST['telephone']);
+                $mdp = addslashes($_POST['mdp']);
+                $pseudo = addslashes($_POST['pseudo']);
+                $avatar = addslashes($_POST['avatar']);
+                $age = addslashes($_POST['age']);
+                $date_naissance = addslashes($_POST['date_de_naiss']);
+                $sexe = addslashes($_POST['sexe']);
+                $etat_civil = addslashes($_POST['etat_civil']);
+                $adresse = addslashes($_POST['adresse']);
+                $code_postal = addslashes($_POST['code_postal']);
+                $ville = addslashes($_POST['ville']);
+                $pays = addslashes($_POST['pays']);
+                $site_web = addslashes($_POST['site_web']);
+                $pdo -> exec("UPDATE INTO t_utilisateurs VALUES (NULL, '$utilisateur', '$prenom', '$nom', '$email', '$telephone', '$mdp', '$pseudo', '$avatar', '$age', '$date_naissance', '$sexe', '$etat_civil', '$adresse', '$code_postal', '$ville', '$pays', '$site_web', '1')");
+                //mettre $id_utilisateur quand on l'aura dans la variable de session
+                header("location: utilisateur.php");
+                exit();
+            } // ferme le if n'est pas vide
+    } // ferme le if(isset) du form
+
+// Suppression d'un utilisateur
+if (isset($_GET['id_utilisateur'])) { // on récupère la comp. par son id dans l'url
+    $efface =  $_GET['id_utilisateur'];
+    $resultat = "DELETE FROM t_utilisateurs WHERE id_utilisateur = '$efface'";
+    $pdo -> query($resultat); // on peut avec exec aussi si on veut
+    header("location: utilisateur.php"); // pour revenir sur la page
+} // ferme le if(isset)
+
+//inclusion du header
+require('inc/header.inc.php');
+
+?>
+<hr>
+    <div class="panel panel-info">
+        <div class="panel-heading text-center"><b>Liste des Utilisateurs</b></div>
+    </div>
+<hr>
+
+<div class="row">
+<div class="col-sm-4 col-md-6 col-md-offset-3">
+    <div class="thumbnail">
+
+        <div class="caption">
+            <div class="panel panel-info">
+                <div class="panel-heading">J'ai <?= $nbr_utilisateur;?> utilisateur<?= ($nbr_utilisateur>1)?'s' : ''?></div>
+            </div>
+            <img src="..." alt="..."/>
+                <?php while ($ligne_utilisateur = $resultat -> fetch()) : ?>
+                    <h3><?= $ligne_utilisateur['nom'];?> <?= $ligne_utilisateur['prenom'];?></h3>
+                    <p><?= $ligne_utilisateur['nom'];?></p>
+                    <p><?= $ligne_utilisateur['prenom'];?></p>
+                    <p class="hidden"><?= $ligne_utilisateur['id_utilisateur'];?></p>
+                    <p><?= $ligne_utilisateur['email'];?></p>
+                    <p><?= $ligne_utilisateur['telephone'];?></p>
+                    <p><?= $ligne_utilisateur['mdp'];?></p>
+                    <p><?= $ligne_utilisateur['pseudo'];?></p>
+                    <p><?= $ligne_utilisateur['age'];?></p>
+                    <p><?= $ligne_utilisateur['date_naissance'];?></p>
+                    <p><?= $ligne_utilisateur['sexe'];?></p>
+                    <p><?= $ligne_utilisateur['etat_civil'];?></p>
+                    <p><?= $ligne_utilisateur['adresse'];?></p>
+                    <p><?= $ligne_utilisateur['code_postal'];?></p>
+                    <p><?= $ligne_utilisateur['ville'];?></p>
+                    <p><?= $ligne_utilisateur['pays'];?></p>
+                    <p><?= $ligne_utilisateur['site_web'];?></p>
+                    <hr>
+                <td><a href="modification_utilisateur.php?id_utilisateur=<?= $ligne_utilisateur['id_utilisateur'];?>"><button type="button" class="btn btn-block btn-warning"><span class="glyphicon glyphicon-pencil" aria-hidden="true">Modifier</span></button></a></td>
+                <!-- <td><a href="utilisateur.php?id_utilisateur=<?= $ligne_utilisateur['id_utilisateur'];?>"><button type="button" class="btn btn-danger"><span class="glyphicon glyphicon-pencil" aria-hidden="true">Supprimer</span></button></a></td> -->
+
+                <?php endwhile ?>
+        </div>
+    </div>
+
+    </div>
+</div>
+<hr>
+
+        <!-- <div class="panel-footer text-center">
+            <a href="inscription.php">Insertion d'un nouvel utilisateur</a>
+        </div> -->
+
+    <?php require('inc/footer.inc.php');?>
